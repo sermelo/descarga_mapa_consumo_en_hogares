9285ddb56469fcbf9c19557218b379350424c9fe
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-04-15 20:08:28 +0100

    The ConsumeSite will save write output to file

--- downloadToJson.py
+++ downloadToJson.py
@@ -9,21 +9,21 @@ from lib.ConsumeSite import ConsumeSite
 MAPA_URL = "https://www.mapa.gob.es/"
 GLOBAL_CONSUME_URL = MAPA_URL + "app/consumo-en-hogares/"
 
-CONSUME_URLS = (GLOBAL_CONSUME_URL + "consulta04.asp",
-#                GLOBAL_CONSUME_URL + "consulta05.asp",
-#                GLOBAL_CONSUME_URL + "consulta06.asp",
-#                GLOBAL_CONSUME_URL + "consulta10.asp",
-#                GLOBAL_CONSUME_URL + "consulta11.asp",
+CONSUME_URLS = (
+                GLOBAL_CONSUME_URL + "consulta04.asp",
+                GLOBAL_CONSUME_URL + "consulta05.asp",
+                GLOBAL_CONSUME_URL + "consulta06.asp",
+                GLOBAL_CONSUME_URL + "consulta10.asp",
+                GLOBAL_CONSUME_URL + "consulta11.asp",
                )
 
-data = []
+generated_files = []
+# TODO: Parallelize this for
 for site_url in CONSUME_URLS:
     consume_site = ConsumeSite()
-    data.extend(consume_site.get_data(url = site_url))
+    file_name = "data-{0}.json".format(uuid.uuid4().hex)
+    generated_files.append(file_name)
+    print("Writing {0} data to {1} file".format(site_url, file_name))
+    consume_site.get_data(site_url, file_name)
 
-file_name = "data-{0}.json".format(uuid.uuid4().hex)
-with io.open(file_name, 'w', encoding='utf8') as json_file:
-    json.dump(data, json_file, ensure_ascii=False)
-
-    print("Data writed to file: {0}".format(file_name))
-
+print("List of generated files: {0}". format(generated_files))
