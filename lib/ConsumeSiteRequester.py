517fd36da87fa264fd80481936c227de63fcf7b4
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2019-04-15 20:04:35 +0100

    Get requests combination by file and save them to file

--- lib/ConsumeSiteRequester.py
+++ lib/ConsumeSiteRequester.py
@@ -16,45 +16,51 @@ class ConsumeSiteRequester(object):
     def __init__(self):
         options = Options()
         options.add_argument('--headless')
-        self.driver = webdriver.Firefox(options=options)
+        self.driver = None
 
     def __del__(self):
         self.driver.quit()
 
-    def process_file(self, input_file):
+    def process_file(self, input_file, output_file):
         with open(input_file, 'r') as json_file:
             combinations = json.load(json_file)
         print("Number of requests to do: {0}".format(len(combinations["requests"])))
-        data = self.__request_combinations(combinations)
-        return data
+        self.__request_combinations(combinations, output_file)
 
-    def __request_combinations(self, combinations):
+    def __request_combinations(self, combinations, output_file):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Firefox(options=options)
         self.driver.get(combinations["url"])
-        data = []
+
         pending_combinations = combinations["requests"]
+        f = open(output_file, 'w')
+        c = 0
         while len(pending_combinations) > 0:
             failed_combinations = []
             for combination in pending_combinations:
-                print("{0} Options to request: \n Category: {1}\n Period: {2}\n Region: {3}".format(time.strftime("%d %H:%M:%S"), combination["category"], combination["period"], combination["region"]))
+                c += 1
+                print(str(c))
+                if c == 20:
+                    c = 0
+                    self.driver.close()
+                    print("Closing the driver")
+                    self.driver = webdriver.Firefox(options=options)
                 try:
-                    data.extend(self.__request_data(combination["category"], combination["period"], combination["region"]))
+                    self.driver.get(combinations["url"])
+                    print("{0} Options to request: \n Category: {1}\n Period: {2}\n Region: {3}".format(time.strftime("%d %H:%M:%S"), combination["category"], combination["period"], combination["region"]))
+                    data = self.__request_data(combination["category"], combination["period"], combination["region"])
+                    f.write(str(json.dumps(data, ensure_ascii=False)))
+                    f.write("\n")
                     combination["done"] = True
-                except NoSuchElementException as err:
+                except Exception as err:
                     print("NoSuchElementException error: {0}".format(err))
                     print("Failed to request: \n Category: {0}\n Period: {1}\n Region: {2}".format(combination["category"], combination["period"], combination["region"]))
                     failed_combinations.append(combination)
-                except ElementClickInterceptedException as err:
-                    print("ElementClickInterceptedException error: {0}".format(err))
-                    print("Failed to request: \n Category: {0}\n Period: {1}\n Region: {2}".format(combination["category"], combination["period"], combination["region"]))
-                    failed_combinations.append(combination)
-                except UnexpectedTagNameException  as err:
-                    print("UnexpectedTagNameException error: {0}".format(err))
-                    print("Failed to request: \n Category: {0}\n Period: {1}\n Region: {2}".format(combination["category"], combination["period"], combination["region"]))
-                    failed_combinations.append(combination)
+                    time.sleep(60)
             print("Failed combinations: {0}".format(failed_combinations))
             pending_combinations = failed_combinations
-
-        return data
+        f.close()
 
     def __request_data(self, category, period, region):
         data = []
@@ -64,11 +70,9 @@ class ConsumeSiteRequester(object):
              "Mes": period.split("/")[0],
              "Año": period.split("/")[1],
              "Región": region,}
-        
+
         self.driver.find_element_by_name("boton1").click()
         parsed_data = self.__parse_data(self.driver.find_element_by_xpath("/html/body/div/div/div[1]/div[2]/div/div[2]/div/div[13]/div/table[2]/tbody"))
-        self.driver.back()
-        
         for registry in parsed_data:
             registry.update(aditional_data)
             data.append(registry)
